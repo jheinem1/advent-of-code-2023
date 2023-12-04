1caf131d456c1f668f92754a9f90d7f1a7da9204
jheinem <jheinem01@icloud.com>
2023-12-04 11:16:19 -0800

    format day 1 with prettier

--- day-one/index.ts
+++ day-one/index.ts
@@ -1,20 +1,22 @@
 import { readFile } from "fs/promises";
 
 const dayOne = async () => {
-    const input = (await readFile("./day-one/input.txt", "utf-8")).split("\n");
-    const numbers = input.map((line) => {
-        let firstDigit: undefined | string;
-        let lastDigit: undefined | string;
-        for (const match of line.matchAll(/\d/g)) {
-            firstDigit = firstDigit ?? match[0];
-            lastDigit = match[0];
-        }
-        if (firstDigit && lastDigit) {
-            return parseInt(firstDigit.concat(lastDigit));
-        }
-        throw "No numbers found";
-    });
-    return numbers.reduce((a, b) => a + b);
-}
+  const input = (await readFile("./day-one/input.txt", "utf-8")).split("\n");
+  const numbers = input.map((line) => {
+    let firstDigit: undefined | string;
+    let lastDigit: undefined | string;
+    for (const match of line.matchAll(/\d/g)) {
+      firstDigit = firstDigit ?? match[0];
+      lastDigit = match[0];
+    }
+    if (firstDigit && lastDigit) {
+      return parseInt(firstDigit.concat(lastDigit));
+    }
+    throw "No numbers found";
+  });
+  return numbers.reduce((a, b) => a + b);
+};
 
-dayOne().then((solution) => console.log(solution)).catch((err) => console.error(err));
+dayOne()
+  .then((solution) => console.log(solution))
+  .catch((err) => console.error(err));
