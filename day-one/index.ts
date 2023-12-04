95f2ec34f9faa1082cc71cd8ce56b29952c12872
jheinem <jheinem01@icloud.com>
2023-12-03 23:02:41 -0800

    Add code to read and calculate sum of numbers from input file

---
+++ day-one/index.ts
@@ -0,0 +1,20 @@
+import { readFile } from "fs/promises";
+
+const dayOne = async () => {
+    const input = (await readFile("./day-one/input.txt", "utf-8")).split("\n");
+    const numbers = input.map((line) => {
+        let firstDigit: undefined | string;
+        let lastDigit: undefined | string;
+        for (var match of line.matchAll(/\d/g)) {
+            firstDigit = firstDigit ?? match[0];
+            lastDigit = match[0];
+        }
+        if (firstDigit && lastDigit) {
+            return parseInt(firstDigit.concat(lastDigit));
+        }
+        throw "No numbers found";
+    });
+    return numbers.reduce((a, b) => a + b);
+}
+
+dayOne().then((solution) => console.log(solution)).catch((err) => console.error(err));
