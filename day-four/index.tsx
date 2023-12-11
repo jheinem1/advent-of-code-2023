6531b3fd0aafc3bb45733f0ae47b49fb2ca85d71
jheinem <jheinem01@icloud.com>
2023-12-10 22:53:41 -0700

    Implement solution for day 4 part 1

--- day-four/index.tsx
+++ day-four/index.tsx
@@ -1,7 +1,62 @@
 import { readFile } from "fs/promises";
 
+interface Scratchcard {
+  id: number;
+  winningNumbers: number[];
+  cardNumbers: number[];
+}
+
+const parseScratchcard = (line: string): Scratchcard => {
+  const [card, numbers] = line.split(":");
+  const [winningNumbers, cardNumbers] = numbers.split("|").map(parseNumberList);
+  return {
+    id: parseInt(card.split(" ")[1]),
+    winningNumbers,
+    cardNumbers,
+  };
+};
+
+const parseNumberList = (numberList: string): number[] => {
+  const numbersMatch = numberList.matchAll(/\d+/g);
+  const numbersList = new Array<number>();
+  for (const numberMatch of numbersMatch) {
+    numbersList.push(parseInt(numberMatch[0]));
+  }
+  return numbersList;
+};
+
+const getWinningNumbersFromScratchcard = (
+  scratchcard: Scratchcard
+): number[] => {
+  const winningNumbers = new Array<number>();
+  for (const cardNumber of scratchcard.cardNumbers) {
+    for (const winningNumber of scratchcard.winningNumbers) {
+      if (cardNumber === winningNumber) {
+        winningNumbers.push(cardNumber);
+      }
+    }
+  }
+  return winningNumbers;
+};
+
+const calculateScore = (winningNumbers: number[]): number => {
+  if (winningNumbers.length === 0) {
+    return 0;
+  }
+  let score = 1;
+  for (let i = 1; i < winningNumbers.length; i++) {
+    score *= 2;
+  }
+  return score;
+};
+
 const partOne = async () => {
   const input = (await readFile("./day-four/input.txt", "utf-8")).split("\n");
+  const scratchcards = input.map(parseScratchcard);
+  console.log(scratchcards);
+  const winningNumbers = scratchcards.map(getWinningNumbersFromScratchcard);
+  console.log(winningNumbers);
+  return winningNumbers.map(calculateScore).reduce((a, b) => a + b);
 };
 
 partOne()
